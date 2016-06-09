cd535039feae323fb6fbdb53724bbe4f6884a6f3
PMhee <tanakorn@NontCp.local>
2016-06-09 16:54:02 +0700

    update

--- LenkilaSportClub/UserViewController.swift
+++ LenkilaSportClub/UserViewController.swift
@@ -21,15 +21,23 @@ class UserViewController: UIViewController {
     }
     func gatherUser(){
         let user = User.allObjects()
-        for i in 0...user.count-1 {
-            userArray.append(user[i] as! User)
+        if user.count > 0 {
+            for i in 0...user.count-1 {
+                userArray.append(user[i] as! User)
+            }
         }
     }
     func genUserTable(){
-        for i in 0...userArray.count-1 {
-            let button = UIButton(frame: CGRectMake(0,124+(40*CGFloat(i)),700,40))
-            button.backgroundColor = UIColor.grayColor()
-            scroll_view.addSubview(button)
+        if userArray.count > 0 {
+            for i in 0...userArray.count-1 {
+                let button = UIButton(frame: CGRectMake(0,124+(40*CGFloat(i)),700,40))
+                if i % 2 == 1 {
+                    button.backgroundColor = UIColor.whiteColor()
+                }else{
+                button.backgroundColor = UIColor(red: 232/255, green: 233/255, blue: 232/255, alpha: 1.0)
+                }
+                scroll_view.addSubview(button)
+            }
         }
     }
     override func didReceiveMemoryWarning() {
@@ -37,14 +45,14 @@ class UserViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-                return UIInterfaceOrientationMask.Landscape
+        return UIInterfaceOrientationMask.Landscape
     }
     /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
 }
