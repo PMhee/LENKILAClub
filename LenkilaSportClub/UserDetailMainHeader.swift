f46588e21cf03e03ffbb8b331f2443ed9487e2c5
Peeranut Mahatham <robin_43499@hotmail.com>
2016-06-09 16:44:27 +0700

    โครง User Detail

---
+++ LenkilaSportClub/UserDetailMainHeader.swift
@@ -0,0 +1,47 @@
+//
+//  UserDetailMainHeader.swift
+//  LenkilaClub
+//
+//  Created by Peeranut Mahatham on 6/9/2559 BE.
+//  Copyright © 2559 Tanakorn. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailMainHeader: UITableViewCell {
+    
+    @IBOutlet var number: UILabel!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var nickname: UILabel!
+    @IBOutlet var gender: UILabel!
+    @IBOutlet var age: UILabel!
+    @IBOutlet var workplace: UILabel!
+    @IBOutlet var numberOfTimes: UILabel!
+    @IBOutlet var time: UILabel!
+    @IBOutlet var contact: UILabel!
+    @IBOutlet var price: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.number.adjustsFontSizeToFitWidth = true;
+        self.name.adjustsFontSizeToFitWidth = true;
+        self.nickname.adjustsFontSizeToFitWidth = true;
+        self.gender.adjustsFontSizeToFitWidth = true;
+        self.age.adjustsFontSizeToFitWidth = true;
+        self.workplace.adjustsFontSizeToFitWidth = true;
+        self.numberOfTimes.adjustsFontSizeToFitWidth = true;
+        self.time.adjustsFontSizeToFitWidth = true;
+        self.contact.adjustsFontSizeToFitWidth = true;
+        self.price.adjustsFontSizeToFitWidth = true;
+                
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+
+}
--- LenkilaSportClub/UserViewController.swift
+++ LenkilaSportClub/UserViewController.swift
@@ -7,35 +7,52 @@
 //
 import UIKit
 import Realm
+
 class UserViewController: UIViewController {
+    
     @IBOutlet weak var scroll_view: UIScrollView!
     var userArray = [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
         gatherUser()
         genUserTable()
     }
+    
     func gatherUser(){
         let user = User.allObjects()
-        for i in 0...user.count-1 {
-            userArray.append(user[i] as! User)
+        if user.count > 0{
+            for i in 0...user.count-1 {
+                userArray.append(user[i] as! User)
+        }
+        
         }
     }
+    
     func genUserTable(){
-        for i in 0...userArray.count-1 {
-            let button = UIButton(frame: CGRectMake(0,124+(40*CGFloat(i)),700,40))
-            button.backgroundColor = UIColor.grayColor()
-            scroll_view.addSubview(button)
+        if userArray.count>0{
+            for i in 0...userArray.count-1 {
+                let button = UIButton(frame: CGRectMake(0,124+(40*CGFloat(i)),700,40))
+                button.backgroundColor = UIColor.grayColor()
+                scroll_view.addSubview(button)
+            }
+
         }
+        
+    
+    
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
                 return UIInterfaceOrientationMask.Landscape
     }
